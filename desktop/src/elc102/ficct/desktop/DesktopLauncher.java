bc2faad017bb6e08358d3630301fb5d999c24391
RenatoAlvarezO <renatoalvarez96@gmail.com>
2022-01-03 22:40:07 -0400

    Primer Commit

---
+++ desktop/src/elc102/ficct/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package elc102.ficct.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import elc102.ficct.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
